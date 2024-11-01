4ad6306941b72c34cd56e831cff210d5acb6d399
ILex34I <osipov.aleksey.01@gmail.com>
2024-11-01 19:16:05 +0300

    lesson_5_1_2

--- Module_5/module_5_1.py
+++ Module_5/module_5_1.py
@@ -14,6 +14,14 @@ class House:
         else:
             print('Такого этажа не существует')
 
+    def __len__(self):
+
+        return self.number_of_floors
+
+    def __str__(self):
+
+        return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+
 
 
 
@@ -24,6 +32,18 @@ h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 
 h2.go_to(3)
+
+# __str__
+
+print(h1)
+
+print(h2)
+
+# __len__
+
+print(len(h1))
+
+print(len(h2))
 
 
 
